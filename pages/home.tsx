bfabaaf6ad16934e94c46db3c191e48dd56a347a
TUMnatthapon <natthapon.tum01@gmail.com>
2022-06-20 01:39:23 +0700

    Update home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,8 +1,29 @@
+import Image from "next/image";
 const Home = () => {
   return (
     <>
       <div className="content-page">
-        <h1>Home</h1>
+        <div className="home-content">
+          <div className="home-content--greeting">
+            HI ALL , I&rsquo;M TUM . . .
+            <span className="wave-emoji">
+              <Image src="/hello.png" alt="👋" width={50} height={50} />
+            </span>
+            <div>
+              NATTHAPON <span className="home-content--font-color"> CHUAISENG</span>
+            </div>
+            <button>About Me !</button>
+          </div>
+          <div className="img-wrapper">
+            <Image
+              className="img-wrapper--profile"
+              src="/profile-home.jpg"
+              alt="profile-home"
+              width={350}
+              height={350}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Header from "../components/Navbar";
 import Home from "./home";
 
 const App = () => {
