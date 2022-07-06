5bf2a49578a3d359c5b82df9abfae38e7bc670e1
TUMnatthapon <natthapon.tum01@gmail.com>
2022-07-06 21:56:29 +0700

    Update SkillsPage

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -2,6 +2,7 @@ import IconC from "../components/icon-c";
 import IconCPP from "../components/icon-cpp";
 import IconNextJs from "../components/icon-nextjs";
 import Image from "next/image";
+import ComputerPic from "../public/Dev-pic.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import {
@@ -12,73 +13,66 @@ import {
   faReact,
   faNode,
   faPython,
-  faBootstrap
+  faBootstrap,
 } from "@fortawesome/free-brands-svg-icons";
 
 const Skills = () => {
   return (
     <>
       <div id="skills">&nbsp;</div>
-      <div className="content-page">
+      <div className="skills">
         <div className="titile-page">
-        <FontAwesomeIcon icon={faLaptopCode} /> Skills
+          <FontAwesomeIcon icon={faLaptopCode} /> Skills
         </div>
-        <div className="skills">
-          <div>
-            <Image
-              src="/profile-home.jpg"
-              alt="profile-home"
-              width={500}
-              height={500}
-            />
-          </div>
-          <ul className="skills--wrapper">
-            <li>
-              <IconC />
-              <p>C</p>
-            </li>
-            <li>
-              <IconCPP />
-              <p>C++</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faHtml5} size="4x" />
-              <p>HTML5</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faCss3Alt} size="4x" />
-              <p>CSS3</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faSass} size="4x" />
-              <p>Sass</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faJsSquare} size="4x" />
-              <p>JavaScript</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faNode} size="4x" />
-              <p>NodeJS</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faReact} size="4x" />
-              <p>ReactJS</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faBootstrap} size="4x" />
-              <p>Bootstrap</p>
-            </li>
-            <li>
-              <FontAwesomeIcon icon={faPython} size="4x" />
-              <p>Python</p>
-            </li>
-            <li>
-              <IconNextJs />
-              <p>NextJS</p>
-            </li>
-          </ul>
+        <div>
+          <Image src={ComputerPic} alt="Photo" />
         </div>
+        <ul className="skills--wrapper">
+          <li>
+            <IconC />
+            <p>C</p>
+          </li>
+          <li>
+            <IconCPP />
+            <p>C++</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faHtml5} size="4x" />
+            <p>HTML5</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCss3Alt} size="4x" />
+            <p>CSS3</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSass} size="4x" />
+            <p>Sass</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faJsSquare} size="4x" />
+            <p>JavaScript</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faNode} size="4x" />
+            <p>NodeJS</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faReact} size="4x" />
+            <p>ReactJS</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBootstrap} size="4x" />
+            <p>Bootstrap</p>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPython} size="4x" />
+            <p>Python</p>
+          </li>
+          <li>
+            <IconNextJs />
+            <p>NextJS</p>
+          </li>
+        </ul>
       </div>
     </>
   );
